5270f36c954a633eec77d0f3bfd95efa1c07ca6f
JP Oliveira <joaopedro.costa.oliveira@gmail.com>
2021-10-03 18:54:13 -0400

    pseudocode for 35

---
+++ src/main/java/exercise35/solution35.java
@@ -0,0 +1,25 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 35 Solution
+ *  Copyright 2021 Joao Pedro Oliveira
+ */
+
+package exercise35;
+
+import java.util.*;
+
+public class solution35 {
+
+    /*
+    Set up an arraylist that will take in all names input by the user
+
+    in a do/while loop ask the user to input a name until they put in a blank input
+
+    randomly choose a winner from the set arraylist of names that the user put
+
+    print "And the winner is...." +nameList;
+     */
+
+    
+
+
+}
